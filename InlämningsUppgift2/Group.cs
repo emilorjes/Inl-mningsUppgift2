9b4a0c51e37fbc14682305987965f3493d010da8
Emil Örjes <emil.orjes@hotmail.com>
2020-12-11 12:49:17 +0100

    Lagt till Fields och Property i basklassen Group.

--- InlämningsUppgift2/Group.cs
+++ InlämningsUppgift2/Group.cs
@@ -6,5 +6,101 @@ namespace InlämningsUppgift2
 {
     class Group
     {
+        //----------------------------------------
+        // Fields (string)
+        //----------------------------------------
+        private string name;
+        private string homeTown;
+        private string hobby;
+        private string favoriteFood;
+        private string favoriteDrink;
+        private string favoriteBand;
+        private string codeMotivation;
+        //----------------------------------------
+
+
+
+
+        //----------------------------------------
+        // Fields (int)
+        //----------------------------------------
+        private int age;
+        private int height;
+        private int numberOfKids;
+        //----------------------------------------
+
+
+
+        //----------------------------------------
+        // Property (string)
+        //----------------------------------------
+
+        public string Name
+        {
+            get { return name; }                         
+            set { name = value; }
+        }
+        //----------------------------------------
+        public string HomeTown
+        {
+            get { return homeTown; }
+            set { homeTown = value; }
+        }
+        //----------------------------------------
+        public string Hobby
+        {
+            get { return hobby; }
+            set { hobby = value; }
+        }
+        //----------------------------------------
+        public string FavoriteFood
+        {
+            get { return favoriteFood; }
+            set { favoriteFood = value; }
+        }
+        //----------------------------------------
+        public string FavoriteDrink
+        {
+            get { return favoriteDrink; }
+            set { favoriteDrink = value; }
+        }
+        //----------------------------------------
+        public string FavoriteBand
+        {
+            get { return favoriteBand; }
+            set { favoriteBand = value; }
+        }
+        //----------------------------------------
+        public string CodeMotivation
+        {
+            get { return codeMotivation; }
+            set { codeMotivation = value; }
+        }
+        //----------------------------------------
+
+
+
+
+        //----------------------------------------
+        // Property (int)                                
+        //----------------------------------------
+        public int Age
+        {
+            get { return age; }
+            set { age = value; }
+        }
+        //----------------------------------------
+        public int Height
+        {
+            get { return height; }
+            set { height = value; }
+        }
+        //----------------------------------------
+        public int NumberOfKids
+        {
+            get { return numberOfKids; }
+            set { numberOfKids = value; }
+        }
+        //----------------------------------------
     }
 }
